c077a60b0a0b7b454bf4a346c391e13249128b47
Odell Purifoy <opurifoy@springhealth.com>
2022-07-11 20:16:54 -0400

    Added custome user attributes to application controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, 
          :registerable,
          :recoverable, 
-         :rememberable, 
+         :rememberable,
          :validatable
 end
