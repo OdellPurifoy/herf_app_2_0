3bdb429389653b4dcac47273450c10fcad58d03a
Odell Purifoy <opurifoy@springhealth.com>
2022-08-12 21:09:13 -0400

    Removed opening and closing times

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,16 +10,5 @@ module ApplicationHelper
   #     'warn' => 'notification is-warning is-light'
   #   }
   #   bulma_alert_class[level]
-  # end
-
-  def time_select_options(step=15)
-    tod = Tod::TimeOfDay.new 0
-    times = []
-    96.times do
-      times << [tod.strftime("%l:%M %P"), tod]
-      tod = tod + step.minutes
-    end
-
-    times
-  end
+  # end  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_13_000508) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_13_010826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,8 +80,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_13_000508) do
     t.boolean "featured", default: false
     t.boolean "outside_drinks_allowed", default: false
     t.boolean "outside_cigars_allowed", default: false
-    t.time "opening_time"
-    t.time "closing_time"
     t.index ["slug"], name: "index_lounges_on_slug", unique: true
     t.index ["user_id"], name: "index_lounges_on_user_id"
   end
