e60452e5b197c96b7d6ff3c096dcb31b124771de
Odell Purifoy <opurifoy@springhealth.com>
2022-08-26 20:45:13 -0400

    Fixed events index pages for lounge events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
 
   # GET /events or /events.json
   def index
-    @events = @lounge.events
+    @events = Event.all
   end
 
   # GET /events/1 or /events/1.json
