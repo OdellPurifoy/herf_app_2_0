88d8ba21af23482767ede18136f568a93050ef0d
Odell Purifoy <opurifoy@springhealth.com>
2022-09-10 18:44:11 -0400

    Added follow feature for users to lounges

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   resources :lounges do
     resources :events, shallow: true
   end
-  
+
+  resources :lounges, only: :index do
+    member do
+      post 'toggle_favorite', to: "lounges#toggle_favorite"
+    end
+  end
+   
   devise_scope :user do
     # Redirects signing out users back to sign-in
     get "users", to: "devise/sessions#new"
