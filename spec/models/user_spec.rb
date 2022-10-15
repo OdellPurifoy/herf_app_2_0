09f88d2fb39674fa3ab09ee2386bea91b8b1601f
Odell Purifoy <opurifoy@springhealth.com>
2022-10-15 19:34:14 -0400

    Updated user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,6 +26,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Database columns' do
+    it { should have_db_column(:admin).of_type(:boolean) }
     it { should have_db_column(:email).of_type(:string) }
     it { should have_db_column(:encrypted_password).of_type(:string) }
     it { should have_db_column(:first_name).of_type(:string) }
