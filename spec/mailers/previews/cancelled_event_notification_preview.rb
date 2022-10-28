0edf71d02fadd8a119d5b238abeac67a97226c91
Odell Purifoy <opurifoy@springhealth.com>
2022-10-28 19:55:21 -0400

    Updated cancellation mailer preview spec

--- spec/mailers/previews/cancelled_event_notification_preview.rb
+++ spec/mailers/previews/cancelled_event_notification_preview.rb
@@ -3,6 +3,6 @@ class CancelledEventNotificationPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/cancelled_event_notification/cancel_notify_followers
   def cancel_notify_followers
-    CancelledEventNotificationMailer.cancel_notify_followers
+    CancelledEventNotificationMailer.with(event: Event.last).cancel_notify_followers
   end
 end
