b288f080396045b7d40b275e6e0cd8abf4e80a12
Odell Purifoy <opurifoy@springhealth.com>
2022-11-12 18:35:08 -0500

    Added after update hooks to special offer model

--- app/mailers/update_special_offer_mailer.rb
+++ app/mailers/update_special_offer_mailer.rb
@@ -1,5 +1,6 @@
-class UpdateSpecialOfferMailer < ApplicationMailer
+# frozen_string_literal: true
 
+class UpdateSpecialOfferMailer < ApplicationMailer
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
