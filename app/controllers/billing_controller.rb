b36e26427a6b4f73870bcbc2bddb38a7172411be
Odell Purifoy <opurifoy@springhealth.com>
2023-01-28 23:58:20 -0500

    Fleshed out billing controller and updated navebar

--- app/controllers/billing_controller.rb
+++ app/controllers/billing_controller.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class BillingController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @portal_session = current_user.payment_processor.billing_portal
+    redirect_to @portal_session.url, allow_other_host: true
+  end
 end
