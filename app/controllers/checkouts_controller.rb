28279820f755807fe9833260ed542d21a3f32afa
Odell Purifoy <opurifoy@springhealth.com>
2023-01-28 20:12:44 -0500

    Fleshed out show action for checkouts controller

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -4,7 +4,16 @@ class CheckoutsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    current_user.set_payment_processor
+    current_user.set_payment_processor :stripe
+    current_user.payment_processor.customer
+
+    @checkout_session = current_user
+                        .payment_processor
+                        .checkout_session(
+                          mode: 'payment',
+                          line_items: 'price_1MVPOeJ7HwUPnCdqF88d3REB',
+                          success_url: checkout_success_url
+                        )
   end
 
   def success; end
