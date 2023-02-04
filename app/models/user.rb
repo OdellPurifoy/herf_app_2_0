77add43c6d9c08a7fe4f6870243769b1ab3486d9
Odell Purifoy <opurifoy@springhealth.com>
2023-02-04 17:24:39 -0500

    Included concern in member model

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,7 @@
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class User < ApplicationRecord
+  include SubcriptionConern
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
