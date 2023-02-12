9aa6ce4de3b3a87714f408499fd1a5badb029f07
Odell Purifoy <opurifoy@springhealth.com>
2023-02-12 17:07:28 -0500

    Added stripe js tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,18 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_stripe_key
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name username phone_number])
     devise_parameter_sanitizer.permit(:account_updated, keys: %i[first_name last_name username phone_number])
+  end
+
+  private
+
+  def set_stripe_key
+    Stripe.api_key = Rails.application.credentials.dig(:stripe, :private_key)
   end
 end
