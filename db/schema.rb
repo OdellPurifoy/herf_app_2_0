7186e42f14ab0d6044a943f174bc7888a5bf2050
Odell Purifoy <opurifoy@springhealth.com>
2023-02-12 16:46:07 -0500

    Ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_12_213406) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_12_214320) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -293,9 +293,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_12_213406) do
     t.string "phone_number"
     t.string "city"
     t.string "country"
-    t.string "subscription_status"
-    t.datetime "subscription_start_date"
-    t.datetime "subscription_end_date"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
