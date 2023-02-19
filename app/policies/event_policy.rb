75070cb1588fb70290bd398edd443605f58a26a1
Odell Purifoy <opurifoy@springhealth.com>
2023-02-19 15:35:10 -0500

    Added event policy and updated events controller

---
+++ app/policies/event_policy.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class EventPolicy < ApplicationPolicy
+  attr_reader :user, :event
+
+  def initialize(user, event)
+    @user = user
+    @event = event
+  end
+
+  def new?
+    @user.admin? || @user.subscribed?
+  end
+
+  def create?
+    @user.admin? || @user.subscribed?
+  end
+
+  def update?
+    @user.admin? || @user.id == event.lounge.user_id
+  end
+
+  def destroy?
+    @user.admin? || @user.id == event.lounge.user_id
+  end
+end
