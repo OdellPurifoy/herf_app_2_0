9117f5360a61044237b6a1f6d7c1fcbd69608b6e
Odell Purifoy <opurifoy@springhealth.com>
2023-03-02 15:40:41 -0500

    Added migration constraints

--- db/migrate/20230302203820_create_notifications.rb
+++ db/migrate/20230302203820_create_notifications.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreateNotifications < ActiveRecord::Migration[7.0]
   def change
     create_table :notifications do |t|
-      t.text :body
-      t.boolean :read
+      t.text :body, null: false
+      t.boolean :read, default: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
