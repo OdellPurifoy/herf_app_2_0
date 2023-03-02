408c6f3d1cceed8e0651fbd22369d501e127da4e
Odell Purifoy <opurifoy@springhealth.com>
2023-03-02 16:34:03 -0500

    Fixed authenticate user before action

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class NotificationsController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user!
   before_action :set_notification, only: %i[show edit update destroy]
 
   def index
