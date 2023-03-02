251f12cb11fca9fa3dfbfdd3d9a2bcbfd9624d7f
Odell Purifoy <opurifoy@springhealth.com>
2023-03-02 15:45:18 -0500

    Destroyed scaffold, created drop table migration for original notifications table

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@
 require 'resque/server'
 
 Rails.application.routes.draw do
-  resources :notifications
   root 'home#index'
   devise_for :users
   devise_scope :user do
--- spec/factories/notifications.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :notification do
-    body { "MyText" }
-    read { false }
-    user { nil }
-  end
-end
