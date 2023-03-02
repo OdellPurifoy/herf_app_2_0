67f30e0a6176b8d942396361e0bdd44e035513a1
Odell Purifoy <opurifoy@springhealth.com>
2023-03-02 15:59:41 -0500

    Updated user model and specs

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -20,5 +20,13 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Database columns' do
+    it { should have_db_column(:body).of_type(:text) }
+    it { should have_db_column(:read).of_type(:boolean) }
+    it { should have_db_index([:user_id]) }
+  end
+
+  describe 'Model Associations' do
+    it { should belong_to(:user) }
+  end
 end
