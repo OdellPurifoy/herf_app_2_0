6aaeffc557b0b8f5c43c81005f7fddbe84d81866
Odell Purifoy <opurifoy@springhealth.com>
2023-03-26 16:08:14 -0400

    Updated remind members email template

--- app/mailers/one_day_reminder_mailer.rb
+++ app/mailers/one_day_reminder_mailer.rb
@@ -1,2 +1,12 @@
 class OneDayReminderMailer < ApplicationMailer
+  def remind_members
+    @membership = params[:membership]
+    @event = params[:event]
+
+    mail(
+      from: 'herf@support.com',
+      to: @membership&.email,
+      subject: "You're invited to our new event!"
+    )
+  end
 end
