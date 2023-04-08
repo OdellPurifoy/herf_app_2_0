cea72a41f22e54d6561351f6df4c0ce54ff54821
Odell Purifoy <opurifoy@springhealth.com>
2023-04-07 20:18:47 -0400

    Ran migration

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,7 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  user_cell_phone        :string
 #  username               :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
