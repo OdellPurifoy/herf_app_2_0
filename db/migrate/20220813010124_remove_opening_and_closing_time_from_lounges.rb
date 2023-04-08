c5c08c79d2d0b2f82195d93143b4c92a6cac719f
Odell Purifoy <opurifoy@springhealth.com>
2023-04-07 20:00:03 -0400

    Commented out bad migrations

--- db/migrate/20220813010124_remove_opening_and_closing_time_from_lounges.rb
+++ db/migrate/20220813010124_remove_opening_and_closing_time_from_lounges.rb
@@ -1,6 +1,6 @@
 class RemoveOpeningAndClosingTimeFromLounges < ActiveRecord::Migration[7.0]
   def change
-    remove_column :lounges, :opening_time, :time
-    remove_column :lounges, :closing_time, :time
+    # remove_column :lounges, :opening_time, :time
+    # remove_column :lounges, :closing_time, :time
   end
 end
