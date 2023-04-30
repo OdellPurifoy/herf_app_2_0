49c00af07afbf9ae5d381032284505ba667bab8f
Odell Purifoy <opurifoy@springhealth.com>
2023-04-29 20:01:52 -0400

    Restored pundit policies

--- app/policies/membership_policy.rb
+++ app/policies/membership_policy.rb
@@ -9,22 +9,18 @@ class MembershipPolicy < ApplicationPolicy
   end
 
   def new?
-    true
-    # @user.admin? || @user.subscribed? || @user.id == @membership.lounge.user_id
+    @user.admin? || @user.subscribed? || @user.id == @membership.lounge.user_id
   end
 
   def create?
-    true
-    # @user.admin? || @user.subscribed? || @user.id == @membership.lounge.user_id
+    @user.admin? || @user.subscribed? || @user.id == @membership.lounge.user_id
   end
 
   def update?
-    true
-    # @user.admin? || @user.id == @membership.lounge.user_id
+    @user.admin? || @user.id == @membership.lounge.user_id
   end
 
   def destroy?
-    true
-    # @user.admin? || @user.id == @membership.lounge.user_id
+    @user.admin? || @user.id == @membership.lounge.user_id
   end
 end
